e172b538db8abaa9ff11cbcaed240429759c8d47
byohannes <byohannes44@gmail.com>
2020-09-21 20:21:55 +0100

    mongodb database connected

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 // app
 const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+// routes
 
 app.get("/", (req, res) => {
   res.send("hello from node");
