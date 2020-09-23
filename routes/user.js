1bcacd7349af2c78e9c9461f27acaccfe48ede3f
byohannes <byohannes44@gmail.com>
2020-09-23 23:50:09 +0100

    updated user routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { signup } = require("../controllers/user");
 
-router.get("/", (req, res) => {
-  res.send("hello from node");
-});
+router.post("/signup", signup);
 
 module.exports = router;
