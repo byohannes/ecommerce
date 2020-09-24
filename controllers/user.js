4fac0edf5bcd7a2d22f6c1a3681eb22c8d564a83
byohannes <byohannes44@gmail.com>
2020-09-24 17:34:02 +0100

    updated user controller to hide salt string and hashed password while creating a user

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,14 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
-      return res.status(400).json({ err });
+      return res.status(400).json({ err: errorHandler(err) });
     }
+    user.salt = undefined;
+    user.hashed_password = undefined;
     res.json({ user });
   });
 };
