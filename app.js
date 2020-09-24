6a23edf3a9d7d415fdc875addd244112afa97f40
byohannes <byohannes44@gmail.com>
2020-09-24 17:31:27 +0100

    updated main server file to includer expree valitor package

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
 require("dotenv").config();
 
 // app
@@ -13,6 +15,7 @@ mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("DB Connected"));
 
@@ -20,6 +23,8 @@ mongoose
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(cors());
 // import routes
 
 const userRoutes = require("./routes/user");
