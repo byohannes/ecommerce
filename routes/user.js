7e771c08ac8dc6c89b01053f3d1b4d17d70860ff
byohannes <byohannes44@gmail.com>
2020-09-28 15:12:53 +0100

    renamed user file with auth

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signup, signin } = require("../controllers/user");
-const { userSignupValidator } = require("../validator");
-
-router.post("/signup", userSignupValidator, signup);
-router.post("/signin", signin);
-
-module.exports = router;
