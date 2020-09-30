04a2211166e29b22ef13c0d71e0f058d9456b3e1
byohannes <byohannes44@gmail.com>
2020-10-01 00:19:49 +0100

    updated product route to  read a product by its Id

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { userById } = require("../controllers/user");
 
+router.post("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
