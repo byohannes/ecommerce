74b5f9af3fcdeb667ccbfae4d0be8a3da68fa5f3
byohannes <byohannes44@gmail.com>
2020-09-30 22:26:09 +0100

    updated the main server file with category routes

--- app.js
+++ app.js
@@ -29,10 +29,12 @@ app.use(cors());
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 // routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000;
 
