17cc2931dffc7f3a033dce306ff2448894b38139
byohannes <byohannes44@gmail.com>
2020-10-02 22:27:58 +0100

    product update created

--- controllers/product.js
+++ controllers/product.js
@@ -89,3 +89,41 @@ exports.remove = (req, res) => {
     });
   });
 };
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    // 1kb = 1000
+    // 1mb = 1000000
+
+    if (files.photo) {
+      // console.log("FILES PHOTO: ", files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb in size",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(result);
+    });
+  });
+};
