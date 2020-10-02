dba318451298cd9f8b7ceb010c2d2dac08a38ba4
byohannes <byohannes44@gmail.com>
2020-10-02 22:13:37 +0100

    created product remove route

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { create, productById, read } = require("../controllers/product");
+const { create, productById, read ,remove} = require("../controllers/product");
 const { userById } = require("../controllers/user");
 
 router.post("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
 
 router.param("userId", userById);
 router.param("productId", productById);
