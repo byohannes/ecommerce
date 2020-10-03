bdef9d768111621f50ac5ed9fe5d181b364b6ae4
byohannes <byohannes44@gmail.com>
2020-10-03 15:03:03 +0100

    fixed a bug on category controller

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.categoryById = (req, res, next, id) => {
