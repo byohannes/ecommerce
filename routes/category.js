2d7acfae27cfcd00f00272c7c0ef5ca0fe0f715a
byohannes <byohannes44@gmail.com>
2020-10-03 14:10:06 +0100

    updated category route to read a category by id

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { create } = require("../controllers/category");
+const { create, categoryById,read } = require("../controllers/category");
 const { userById } = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
 
+router.param("categoryId", categoryById);
 router.param("userId", userById);
 
 module.exports = router;
