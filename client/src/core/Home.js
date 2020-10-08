f863be273da8ab8adcc1fa6efc6bc41e4d759e37
byohannes <byohannes44@gmail.com>
2020-10-09 00:06:06 +0100

    updated home component to include layout

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "./Layout";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+const Home = () => (
+  <Layout
+    title="FullStack React Node MongoDB Ecommerce App"
+    description="Node React E-commerce App"
+    className="container-fluid"
+  >
+      ...
+  </Layout>
+);
 
 export default Home;
