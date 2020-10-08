da21c033762e4b5c894e3e5695f86dc5c531a5a1
byohannes <byohannes44@gmail.com>
2020-10-09 00:06:47 +0100

    updated signin component to include layout

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Layout from "../core/Layout";
 
-const Signin = () => {
-  return <div>Signin</div>;
-};
+
+const Signin = () => (
+    <Layout
+      title="Sigin"
+      description="Sign in to Node React E-commerce App"
+      className="container-fluid"
+    >
+        ...
+    </Layout>
+  );
 
 export default Signin;
