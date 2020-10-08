86fc6db935add5543a876d877c83382ff804a2bb
byohannes <byohannes44@gmail.com>
2020-10-09 00:06:36 +0100

    updated signup component to include layout

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "../core/Layout";
 
-const Signup = () => {
-  return <div>Signup</div>;
-};
+const Signup = () => (
+  <Layout
+    title="Signup"
+    description="Signup in to Node React E-commerce App"
+    className="container-fluid"
+  >
+    ...
+  </Layout>
+);
 
 export default Signup;
