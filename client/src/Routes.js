4b8a61a6676e5cb125e1b756130df0c71e67f083
byohannes <byohannes44@gmail.com>
2020-10-09 00:05:34 +0100

    moved menu component from routes into layout

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
-import Home from "./core/Home";
-import Menu from "./core/Menu";
+import Home from "./core/Home"; 
 
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <Menu />
+    <BrowserRouter> 
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={Signin} />
