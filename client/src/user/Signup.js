df8a9245dc22d21c4b841316ab39042641f8db4d
byohannes <byohannes44@gmail.com>
2020-10-09 00:29:41 +0100

    enviroment variables created

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => (
   <Layout
@@ -7,7 +8,7 @@ const Signup = () => (
     description="Signup in to Node React E-commerce App"
     className="container-fluid"
   >
-    ...
+    {API}
   </Layout>
 );
 
