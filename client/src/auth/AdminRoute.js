48b9df49dff96dbb24cd1429f326d48706f12801
byohannes <byohannes44@gmail.com>
2020-10-23 20:50:03 +0100

    updated admin dashboard

--- client/src/auth/AdminRoute.js
+++ client/src/auth/AdminRoute.js
@@ -3,21 +3,21 @@ import { Route, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./index";
 
 const AdminRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      isAuthenticated() && isAuthenticated().user.role === 1 ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/signin",
-            state: { from: props.location },
-          }}
-        />
-      )
-    }
-  />
+    <Route
+        {...rest}
+        render={props =>
+            isAuthenticated() && isAuthenticated().user.role === 1 ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/signin",
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    />
 );
 
 export default AdminRoute;
