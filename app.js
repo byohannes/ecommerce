378bd95342ea53c038ff73cb0a05c90ff10aba2e
byohannes <byohannes44@gmail.com>
2020-10-24 18:41:46 +0100

    pdated server file to include payment routes

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const expressValidator = require("express-validator");
 require("dotenv").config();
+// import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
+const orderRoutes = require("./routes/order");
 
 // app
 const app = express();
@@ -15,28 +22,23 @@ mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("DB Connected"));
 
-// middleware
+// middlewares
 app.use(morgan("dev"));
-app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
-// import routes
-
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
 
 // routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
+app.use("/api", orderRoutes);
 
 const port = process.env.PORT || 8000;
 
