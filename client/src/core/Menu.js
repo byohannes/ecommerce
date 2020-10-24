c4a881b3e127200ec75c47d26255c82c808a9722
byohannes <byohannes44@gmail.com>
2020-10-24 18:45:13 +0100

    updated cliet  menu

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth";
-//import { itemTotal } from "./cartHelpers";
+import { itemTotal } from "./cartHelpers";
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -37,7 +37,9 @@ const Menu = ({ history }) => (
           to="/cart"
         >
           Cart{" "}
-          <sup>{/* <small className="cart-badge">{itemTotal()}</small> */}</sup>
+          <sup>
+            <small className="cart-badge">{itemTotal()}</small>
+          </sup>
         </Link>
       </li>
 
