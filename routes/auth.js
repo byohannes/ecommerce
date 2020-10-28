309367dba5ad9b58a69cb167d73ce631318ad299
byohannes <byohannes44@gmail.com>
2020-10-28 22:32:51 +0000

    updated user authentication route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const { signup, signin, signout } = require("../controllers/auth");
 const { userSignupValidator } = require("../validator");
 
