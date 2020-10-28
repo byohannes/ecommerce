dd1dffffcd5550f2690c7de497e86b3e2db1118a
byohannes <byohannes44@gmail.com>
2020-10-28 22:31:23 +0000

    updated user roue

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,25 @@
 const express = require("express");
-const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const router = express.Router();
-const { userById, read, update } = require("../controllers/user");
 
-router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory,
+} = require("../controllers/user");
+
+router.get("/secret", requireSignin, (req, res) => {
   res.json({
-    user: req.profile,
+    user: "got here yay",
   });
 });
 
 router.get("/user/:userId", requireSignin, isAuth, read);
 router.put("/user/:userId", requireSignin, isAuth, update);
+router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
+
 router.param("userId", userById);
 
 module.exports = router;
