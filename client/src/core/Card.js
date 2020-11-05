b0fc74ad1eb9cfb60d19b0ba5fb2232394b8542b
byohannes <byohannes44@gmail.com>
2020-11-05 22:00:38 +0000

    changed book price from doller sign into pounde sign

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -109,7 +109,7 @@ const Card = ({
         {shouldRedirect(redirect)}
         <ShowImage item={product} url="product" />
         <p className="card-p  mt-2">{product.description.substring(0, 100)} </p>
-        <p className="card-p black-10">$ {product.price}</p>
+        <p className="card-p black-10">£ {product.price}</p>
         <p className="black-9">
           Category: {product.category && product.category.name}
         </p>
