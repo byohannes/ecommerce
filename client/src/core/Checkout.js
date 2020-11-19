3be35a6678c8a463d0e9181cfe831b62ec17e596
byohannes <byohannes44@gmail.com>
2020-11-19 01:37:36 +0000

    updated total currency sign

--- client/src/core/Checkout.js
+++ client/src/core/Checkout.js
@@ -179,7 +179,7 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
 
   return (
     <div>
-      <h2>Total: ${getTotal()}</h2>
+      <h2>Total: £{getTotal()}</h2>
       {showLoading(data.loading)}
       {showSuccess(data.success)}
       {showError(data.error)}
