80cd5862fb79f35385b610db3410077c4aeb7a58
byohannes <byohannes44@gmail.com>
2020-12-08 20:10:58 +0000

    changed the server port number

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 }
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
